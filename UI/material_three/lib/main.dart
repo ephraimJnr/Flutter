9744efbf74824e273202997d13b2174618502791
ephraim <ephraimndoromuinde@gmail.com>
2023-02-13 11:17:50 +0300

    final project

--- UI/material_three/lib/main.dart
+++ UI/material_three/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 
-import 'destinations.dart';
+import 'animations.dart';
 import 'models/data.dart' as data;
 import 'models/models.dart';
-import 'widgets/email_list_view.dart';
+import 'transitions/list_detail_transition.dart'; // Add import
+import 'widgets/animated_floating_action_button.dart';
 import 'widgets/disappearing_bottom_navigation_bar.dart';
 import 'widgets/disappearing_navigation_rail.dart';
+import 'widgets/email_list_view.dart';
+import 'widgets/reply_list_view.dart'; // Add import
 
 void main() {
   runApp(const MainApp());
@@ -17,7 +20,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true),
       home: Feed(currentUser: data.user_0),
     );
@@ -36,71 +38,108 @@ class Feed extends StatefulWidget {
   State<Feed> createState() => _FeedState();
 }
 
-class _FeedState extends State<Feed> {
+class _FeedState extends State<Feed> with SingleTickerProviderStateMixin {
   late final _colorScheme = Theme.of(context).colorScheme;
   late final _backgroundColor = Color.alphaBlend(
       _colorScheme.primary.withOpacity(0.14), _colorScheme.surface);
+  late final _controller = AnimationController(
+      duration: const Duration(milliseconds: 1000),
+      reverseDuration: const Duration(milliseconds: 1250),
+      value: 0,
+      vsync: this);
+  late final _railAnimation = RailAnimation(parent: _controller);
+  late final _railFabAnimation = RailFabAnimation(parent: _controller);
+  late final _barAnimation = BarAnimation(parent: _controller);
 
   int selectedIndex = 0;
-  bool wideScreen = false;
+  bool controllerInitialized = false;
 
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
 
     final double width = MediaQuery.of(context).size.width;
-    wideScreen = width > 600;
+    final AnimationStatus status = _controller.status;
+    if (width > 600) {
+      if (status != AnimationStatus.forward &&
+          status != AnimationStatus.completed) {
+        _controller.forward();
+      }
+    } else {
+      if (status != AnimationStatus.reverse &&
+          status != AnimationStatus.dismissed) {
+        _controller.reverse();
+      }
+    }
+    if (!controllerInitialized) {
+      controllerInitialized = true;
+      _controller.value = width > 600 ? 1 : 0;
+    }
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Row(
-        children: [
-          if (wideScreen)
-            DisappearingNavigationRail(
-              selectedIndex: selectedIndex,
-              backgroundColor: _backgroundColor,
-              onDestinationSelected: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-            ),
-          Expanded(
-            child: Container(
-              color: _backgroundColor,
-              child: EmailListView(
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (context, _) {
+        return Scaffold(
+          body: Row(
+            children: [
+              DisappearingNavigationRail(
+                railAnimation: _railAnimation,
+                railFabAnimation: _railFabAnimation,
                 selectedIndex: selectedIndex,
-                onSelected: (index) {
+                backgroundColor: _backgroundColor,
+                onDestinationSelected: (index) {
                   setState(() {
                     selectedIndex = index;
                   });
                 },
-                currentUser: widget.currentUser,
               ),
-            ),
+              Expanded(
+                child: Container(
+                  color: _backgroundColor,
+                  // Update from here ...
+                  child: ListDetailTransition(
+                    animation: _railAnimation,
+                    one: EmailListView(
+                      selectedIndex: selectedIndex,
+                      onSelected: (index) {
+                        setState(() {
+                          selectedIndex = index;
+                        });
+                      },
+                      currentUser: widget.currentUser,
+                    ),
+                    two: const ReplyListView(),
+                  ),
+                  // ... to here.
+                ),
+              ),
+            ],
           ),
-        ],
-      ),
-      floatingActionButton: wideScreen
-          ? null
-          : FloatingActionButton(
-              backgroundColor: _colorScheme.tertiaryContainer,
-              foregroundColor: _colorScheme.onTertiaryContainer,
-              onPressed: () {},
-              child: const Icon(Icons.add),
-            ),
-      bottomNavigationBar: wideScreen
-          ? null
-          : DisappearingBottomNavigationBar(
-              selectedIndex: selectedIndex,
-              onDestinationSelected: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-              },
-            ),
+          floatingActionButton: AnimatedFloatingActionButton(
+            animation: _barAnimation,
+            onPressed: () {},
+            child: const Icon(Icons.add),
+          ),
+          bottomNavigationBar: DisappearingBottomNavigationBar(
+            barAnimation: _barAnimation,
+            selectedIndex: selectedIndex,
+            onDestinationSelected: (index) {
+              setState(() {
+                selectedIndex = index;
+              });
+            },
+          ),
+        );
+      },
     );
   }
 }
